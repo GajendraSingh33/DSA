8652f8ee8f0f7c2138ee2e7f5270974e35e8ee33
Gajendra Singh <gajendrasinghrajput917@gmail.com>
2026-08-19 23:23:38 +0530

    Time: 0 ms (100%), Space: 47.3 MB (49.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int xor = n;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {       //
             xor ^= i ^ nums[i];
         }
         return xor;
